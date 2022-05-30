d924bd80e69340c6d10997b953b665b179630c54
Srijan Singh <srijanssj5goku@gmail.com>
2022-05-30 21:05:15 +0530

    WORKING

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Intro from "./components/intro/intro";
+import About from "./components/about/about";
+import ProjectList from "./components/projectList/projectList"
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return <div>
+    <Intro/>
+    <About/>
+    <ProjectList/>
+  </div>;
 };
 
 export default App;
